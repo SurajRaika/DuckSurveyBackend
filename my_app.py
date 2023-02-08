279938fbb98eee76c74b5f2c5d04eb121440443b
SurajRaika <103476230+SurajRaika@users.noreply.github.com>
2023-02-08 15:16:10 +0530

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -1,5 +1,3 @@
-if __name__ == '__main__':
-    from surveyapi.application import create_app
-    app = create_app()
-    app.run()
-    
+
+from surveyapi.application import create_app
+app = create_app()
